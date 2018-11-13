ffd0e371126f2b10ffd0ec069ba2f128e63d175e
DESKTOP-QSIJSJP\47996 <749282666@qq.com>
2018-11-13 15:49:03 +0800

    【完成】 显示空 窗口

---
+++ LearnOpenGL/main.cpp
@@ -0,0 +1,50 @@
+
+#define GLEW_STATIC
+
+#include<GL\glew.h>
+#include<GLFW\glfw3.h>
+
+
+void processInput(GLFWwindow* window);
+
+int main() {
+
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Hello OpenGL", NULL, NULL);
+	if (window == NULL) {
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	glewExperimental = true;
+	if (glewInit() != GLEW_OK) {
+		glfwTerminate();
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+	while (!glfwWindowShouldClose(window))
+	{
+		//Input Event
+		processInput(window);
+		//Rendering
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return -1;
+}
+
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+}
