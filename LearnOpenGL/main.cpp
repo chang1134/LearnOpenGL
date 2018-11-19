d3a6efb71ab438716f73cb4fd89a46bc5f8afb96
DESKTOP-QSIJSJP\47996 <749282666@qq.com>
2018-11-19 09:15:21 +0800

    成功绘制出一个 三角形， 但是颜色确实白色，无法通过调整fragment shader进而调整颜色

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -11,6 +11,19 @@ float vertices[] = {
 	0.5f, -0.5f, 0.0f,
 	0.0f,  0.5f, 0.0f
 };
+
+const char* vertexShaderSource =
+"#version 330 core										  /n"
+"layout(location = 0) in vec3 aPos;						  /n"
+"void main(){											  /n"
+"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);}	  /n";
+
+const char* fragmentShaderSource =
+"#version 330 core										  /n"
+"out vec4 FragColor;									  /n"
+"void main(){											  /n"
+"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);}			  /n";
+
 int main() {
 
 	glfwInit();
@@ -41,13 +54,38 @@ int main() {
 	unsigned int VBO;
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, signed(vertices), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
 
 	while (!glfwWindowShouldClose(window))
 	{
 		//Input Event
 		processInput(window);
-		//Rendering
+		//Rendering Background
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBindVertexArray(VAO);
+		glUseProgram(shaderProgram);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
