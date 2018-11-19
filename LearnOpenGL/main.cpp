2c4a3c551b5d37522743fc17d979cd58403abcfc
DESKTOP-QSIJSJP\47996 <749282666@qq.com>
2018-11-19 17:16:36 +0800

    绘制出两个三角形，并且实现 根据时间进行渐变

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -3,26 +3,41 @@
 
 #include<GL\glew.h>
 #include<GLFW\glfw3.h>
+#include<cmath>
 
 
 void processInput(GLFWwindow* window);
 float vertices[] = {
 	-0.5f, -0.5f, 0.0f,
 	0.5f, -0.5f, 0.0f,
-	0.0f,  0.5f, 0.0f
+	0.0f,  0.5f, 0.0f,
+	//0.5f, -0.5f, 0.0f,
+	//0.0f,  0.5f, 0.0f,
+	0.8f,  0.8f, 0.0f
+};
+
+unsigned int indices[] =
+{
+	0, 1, 2,
+	2, 1, 3
 };
 
 const char* vertexShaderSource =
-"#version 330 core										  /n"
-"layout(location = 0) in vec3 aPos;						  /n"
-"void main(){											  /n"
-"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);}	  /n";
+"#version 330 core										 \n"
+"layout(location = 0) in vec3 aPos;						 \n"
+"out vec4 vertexColor;					  \n"
+"void main(){											  \n"
+"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);	  \n"
+"  	vertexColor = vec4(1.0, 1.0, 0, 1.0);  \n"
+"}	  \n";
 
 const char* fragmentShaderSource =
-"#version 330 core										  /n"
-"out vec4 FragColor;									  /n"
-"void main(){											  /n"
-"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);}			  /n";
+"#version 330 core										  \n"
+"in vec4 vertexColor;									  \n"
+"uniform vec4 ourColor;									  \n"
+"out vec4 FragColor;									  \n"
+"void main(){											  \n"
+"	FragColor = ourColor;}			  \n";
 
 int main() {
 
@@ -46,6 +61,10 @@ int main() {
 	}
 
 	glViewport(0, 0, 800, 600);
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	//CULL BACK
+	//glEnable(GL_CULL_FACE);
+	//glCullFace(GL_BACK);
 
 	unsigned int VAO;
 	glGenVertexArrays(1, &VAO);
@@ -55,6 +74,11 @@ int main() {
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	unsigned int vertexShader;
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -84,8 +108,16 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glBindVertexArray(VAO);
+
+		float timeValue = glfwGetTime();
+		float greenValue = sin(timeValue) / 2.0f + 0.5f;
+		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+		glUniform4f(vertexColorLocation, 0, greenValue, 0, 1.0f);
+
 		glUseProgram(shaderProgram);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		//glDrawArrays(GL_TRIANGLES, 0, 6);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
