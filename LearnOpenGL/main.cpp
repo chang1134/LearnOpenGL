e43d928478e142a1e2528b02dc3a829792aebbe2
DESKTOP-QSIJSJP\47996 <749282666@qq.com>
2019-03-21 10:33:37 +0800

    保存代码

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -1,22 +1,41 @@
-
+#include <iostream>
+#include "Shader.h"
 #define GLEW_STATIC
-
 #include<GL\glew.h>
 #include<GLFW\glfw3.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
-void processInput(GLFWwindow* window);
+float vertices[] = {
+	//     ---- 位置 ----       ---- 颜色 ----     - 纹理坐标 -
+	0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // 右上
+	0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // 右下
+	-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // 左下
+	-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // 左上
+};
+
+unsigned int indices[] = {
+	0,1,2,
+	2,3,0
+};
+
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+}
 
 int main() {
 
 	glfwInit();
-
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Hello OpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "My OpenGL Game", NULL, NULL);
 	if (window == NULL) {
+		printf("Open window failed.");
 		glfwTerminate();
 		return -1;
 	}
@@ -29,6 +48,18 @@ int main() {
 	}
 
 	glViewport(0, 0, 800, 600);
+
+	Shader * myShader = new Shader("vertexSoure", "");
+
+
+
+
+
+
+
+
+
+
 	while (!glfwWindowShouldClose(window))
 	{
 		//Input Event
@@ -42,9 +73,4 @@ int main() {
 	glfwTerminate();
 	return -1;
 }
-
-void processInput(GLFWwindow* window) {
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-		glfwSetWindowShouldClose(window, true);
-	}
-}
+ 
